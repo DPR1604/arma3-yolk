19df0ade23291dfa50e04813d4d40fa60d05ad3c
Gareth Jones <gareth@valhallaonline.info>
2024-04-10 23:02:06 +0100

    added some temp debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -319,6 +319,8 @@ if [[ ${UPDATE_SERVER} == 1 ]]; then
         done
 
         set -x
+        ls -la
+        ls -la ./mods
         # Ensure mod keys are up to date
         echo -e "\n${GREEN}[UPDATE]:${NC} Checking for missing keyfiles" 
         for keyFile in $(find ./mods -name "*.bikey" -type f); do
