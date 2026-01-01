49c19bc055f928e07210623355ff1192e64d77d6
Avril112113 <curtisdude12@gmail.com>
2026-01-01 18:27:56 +0000

    `curl` use ` --compressed` to deal with sometimes gziped data

--- entrypoint.sh
+++ entrypoint.sh
@@ -290,12 +290,12 @@ if [[ ${UPDATE_SERVER} == 1 ]]; then
                 modDir=./mods/@${modID}
 
                 # Get mod's latest update in epoch time from its Steam Workshop changelog page
-                latestUpdate=$(curl -sL https://steamcommunity.com/sharedfiles/filedetails/changelog/$modID | grep '<p id=' | head -1 | cut -d'"' -f2)
+                latestUpdate=$(curl -sL --compressed https://steamcommunity.com/sharedfiles/filedetails/changelog/$modID | grep '<p id=' | head -1 | cut -d'"' -f2)
 
                 # If the update time is valid and newer than the local directory's creation date, or the mod hasn't been downloaded yet, download the mod
                 if [[ ! -d $modDir ]] || [[ ( -n $latestUpdate ) && ( $latestUpdate =~ ^[0-9]+$ ) && ( $latestUpdate > $(find $modDir | head -1 | xargs stat -c%Y) ) ]]; then
                     # Get the mod's name from the Workshop page as well
-                    modName=$(curl -sL https://steamcommunity.com/sharedfiles/filedetails/changelog/$modID | grep 'workshopItemTitle' | cut -d'>' -f2 | cut -d'<' -f1)
+                    modName=$(curl -sL --compressed https://steamcommunity.com/sharedfiles/filedetails/changelog/$modID | grep 'workshopItemTitle' | cut -d'>' -f2 | cut -d'<' -f1)
                     if [[ -z $modName ]]; then # Set default name if unavailable
                         modName="[NAME UNAVAILABLE]"
                     fi
